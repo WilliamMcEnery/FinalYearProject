c949d7c2641467d436a774cfbbe535fc2c35b718
William McEnery <mcenerywilliam@gmail.com>
2021-01-21 20:11:22 +0000

    Created a react project and basic component

---
+++ geo-locator-ui/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// Importing Components
+import Header from "./components/header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
+}
+
+export default App;
---
+++ geo-locator-ui/src/components/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <h1>Geo-locator</h1>
+    )
+}
+
+export default Header
