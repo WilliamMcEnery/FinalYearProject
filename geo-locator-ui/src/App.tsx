b699ed376c2ff0edec4c8db44fd9372c06d47b58
William McEnery <mcenerywilliam@gmail.com>
2021-01-22 21:07:23 +0000

    search component working + created service

--- geo-locator-ui/src/App.tsx
+++ geo-locator-ui/src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 // Importing Components
 import Header from "./components/header";
+import Form from "./components/form";
 
 function App() {
+    const [inputText, setInputText] = useState<string>("");
     return (
-        <div className="App">
-            <Header />
+        <div>
+            <div className="App">
+                <Header/>
+            </div>
+            <div>
+                <Form inputText={inputText} setInputText={setInputText}/>
+            </div>
         </div>
     );
 }
---
+++ geo-locator-ui/src/components/form.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { TweetGeoLocationService } from "../services/tweetGeoLocationService"
+const tweetGeoLocationService = new TweetGeoLocationService()
+
+interface Props {
+    inputText: string;
+    setInputText: (txt: string) => void;
+}
+
+const Form: React.FC<Props> = ({inputText, setInputText}) => {
+    const inputTextHandler = (e: any) => {
+        console.log(e.target.value)
+        setInputText(e.target.value)
+    };
+    const submitButtonHandler = async (e: any) => {
+        e.preventDefault();
+        const res = await tweetGeoLocationService.getLocations(inputText)
+        console.log(res)
+        setInputText("");
+    };
+    return (
+        <form>
+            <input value={inputText} onChange={inputTextHandler} type="text" className="topic-input"/>
+            <button onClick={submitButtonHandler} type="submit">Search</button>
+        </form>
+    )
+}
+
+export default Form
