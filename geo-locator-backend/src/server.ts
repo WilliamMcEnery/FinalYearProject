e62d4ea5b218add2557279847e79c14c87a2a3ba
William McEnery <mcenerywilliam@gmail.com>
2021-01-30 19:53:26 +0000

    create basic websocket server

--- geo-locator-backend/src/server.ts
+++ geo-locator-backend/src/server.ts
@@ -1,7 +1,11 @@
 import * as express from "express";
 import * as path from "path";
+import * as http from "http";
+import * as WebSocket from "ws";
 
 export class Server {
+
+    private server: http.Server
 
     constructor(private app: express.Express) {
         this.app = app;
@@ -11,10 +15,29 @@ export class Server {
         this.app.get("*", (req: express.Request, res: express.Response): void => {
             res.sendFile(path.resolve("./") + "/build/frontend/index.html");
         });
+
+        //initialize a simple http server
+        this.server = http.createServer(this.app);
+
+        //initialize the WebSocket server instance
+        const wss = new WebSocket.Server({ server: this.server });
+
+        wss.on("connection", (ws: WebSocket) => {
+            console.log("A new client Connected!");
+            ws.send("Hello there!");
+
+            ws.on("message", (msg: string) => {
+                console.log("Received a message: " + msg);
+            });
+        });
     }
 
     public run(): void {
         const port = process.env.PORT || 8080;
-        this.app.listen(port, () => console.log("Server listening on port " + port));
+
+        //start our server
+        this.server.listen(port, () => {
+            console.log("Server is listening on port " + port);
+        });
     }
 }
